581f0075ef7efee043234ba15d1ee21ff7612e0e
WadeTheDev <echo1006j@gmail.com>
2022-06-20 13:39:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// 01 - Hello World
+
+// console.log("Hello World !")
+
+// 02 - String 
+
+// const sentence = "My name is Wade" 
+// console.log(sentence)
+
+// 03 - Concaténation
+
+// const name = "Wade"
+
+// console.log("Nice to meet you " + name)
+
+// 04 - String Length
+
+// const sentence = "I'm a new text"
+
+// console.log(sentence.length)
+
+// 05 - Replace
+
+// const food = "Croissant is meh"
+
+// console.log(food.replace("meh","so good"))
+
+// 06 - Up and Down
+
+// const basic = "This is so cool"
+// const basicUp = basic.toUpperCase()
+// const basicDown = basic.toLocaleLowerCase()
+
+// console.log(basic, basicUp, basicDown)
+
+// 07 - Split
+
+// const word = "banana"
+
+// const letters = word.split("")
+
+// console.log(letters)
+
+const age = 20
+const template = `I'm ${age} years old`
+
+console.log(template)
